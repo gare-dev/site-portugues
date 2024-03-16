7a14b3b00fa6bb48440e8a79246d1925efa91747
Guilherme Gare <ggare.contato@gmail.com>
2024-03-16 01:46:02 -0300

    finished

---
+++ script.js
@@ -0,0 +1,77 @@
+const text = document.getElementById("txt");
+
+const original = document.getElementById("original");
+const resumo = document.getElementById("resumo");
+
+const origintext = "fodase"
+
+function originalTxt() {
+
+    text.innerHTML = `
+    Se os tubarões fossem homens, perguntou a filha de sua senhoria ao senhor K., seriam eles mais amáveis para
+    com os peixinhos?
+
+    <br><br>
+
+    Certamente, respondeu o Sr. K. Se os tubarões fossem homens, construiriam no mar grandes gaiolas para os
+    peixes pequenos, com todo tipo de alimento, tanto animal quanto vegetal. Cuidariam para que as gaiolas
+    tivessem sempre água fresca e adoptariam todas as medidas sanitárias adequadas. Se, por exemplo, um peixinho
+    ferisse a barbatana, ser-lhe-ia imediatamente aplicado um curativo para que não morresse antes do tempo.
+    <br><br>
+    Para que os peixinhos não ficassem melancólicos haveria grandes festas aquáticas de vez em quando, pois os
+    peixinhos alegres têm melhor sabor do que os tristes. Naturalmente haveria também escolas nas gaiolas.
+    Nessas escolas os peixinhos aprenderiam como nadar alegremente em direcção à goela dos tubarões. Precisariam
+    saber geografia, por exemplo, para localizar os grandes tubarões que vagueiam descansadamente pelo mar.
+    <br><br>
+    O mais importante seria, naturalmente, a formação moral dos peixinhos. Eles seriam informados de que nada
+    existe de mais belo e mais sublime do que um peixinho que se sacrifica contente, e que todos deveriam crer
+    nos tubarões, sobretudo quando dissessem que cuidam de sua felicidade futura. Os peixinhos saberiam que este
+    futuro só estaria assegurado se estudassem docilmente. Acima de tudo, os peixinhos deveriam rejeitar toda
+    tendência baixa, materialista, egoísta e marxista, e denunciar imediatamente aos tubarões aqueles que
+    apresentassem tais tendências.
+    <br><br>
+    Se os tubarões fossem homens, naturalmente fariam guerras entre si, para conquistar gaiolas e peixinhos
+    estrangeiros. Nessas guerras eles fariam lutar os seus peixinhos, e lhes ensinariam que há uma enorme
+    diferença entre eles e os peixinhos dos outros tubarões. Os peixinhos, proclamariam, são notoriamente mudos,
+    mas silenciam em línguas diferentes, e por isso não se podem entender entre si. Cada peixinho que matasse
+    alguns outros na guerra, os inimigos que silenciam em outra língua, seria condecorado com uma pequena
+    medalha de sargaço e receberia uma comenda de herói.
+    <br><br>
+    Se os tubarões fossem homens também haveria arte entre eles, naturalmente. Haveria belos quadros,
+    representando os dentes dos tubarões em cores magníficas, e as suas goelas como jardins onde se brinca
+    deliciosamente. Os teatros do fundo do mar mostrariam valorosos peixinhos a nadarem com entusiasmo rumo às
+    gargantas dos tubarões. E a música seria tão bela que, sob os seus acordes, todos os peixinhos, como
+    orquestra afinada, a sonhar, embalados nos pensamentos mais sublimes, precipitar-se-iam nas goelas dos
+    tubarões.
+    <br><br>
+    Também não faltaria uma religião, se os tubarões fossem homens. Ela ensinaria que a verdadeira vida dos
+    peixinhos começa no paraíso, ou seja, na barriga dos tubarões.
+    <br><br>
+    Se os tubarões fossem homens também acabaria a ideia de que todos os peixinhos são iguais entre si. Alguns
+    deles se tornariam funcionários e seriam colocados acima dos outros. Aqueles ligeiramente maiores até
+    poderiam comer os menores. Isso seria agradável para os tubarões, pois eles, mais frequentemente, teriam
+    bocados maiores para comer. E os peixinhos maiores detentores de cargos, cuidariam da ordem interna entre os
+    peixinhos, tornando-se professores, oficiais, polícias, construtores de gaiolas, etc.
+    <br><br>
+    Em suma, se os tubarões fossem homens haveria uma civilização no mar.
+</p>`
+}
+
+function resumoTxt() {
+    text.innerHTML = `Neste texto, temos a representação escrita de como seria o mundo se tubarões fossem os governantes e os peixinhos, a população em geral. 
+    <br><br>
+
+    Humanos e animais marinhos são, obviamente, de mundos completamente diferentes. Logo, o autor traz o que vivemos no nosso mundo para o mundo dos animais marinhos.
+    <br><br>
+    Fazendo então essa comparação de mundos distintos, mas com as mesmas circunstâncias, ele exibe tal situação de modo a provar que estamos normalizando algo que não deve ser normalizado.
+    <br><br>
+    Mas só percebemos isso ao nos depararmos com a mesma questão frisada do ponto de vista de outra realidade.
+    <br><br>
+    Isso mostra o quão estamos sendo manipulados por pessoas que dizem querer nosso bem, mas na realidade, nunca querem que estejamos melhor do que elas.
+    <br><br>
+    Em suma, tal texto traz uma crítica para abrir os olhos da população que normaliza a manipulação de indivíduos em busca do próprio poder em prejuízo do bem-estar da população em geral.`
+}
+
+
+
+
